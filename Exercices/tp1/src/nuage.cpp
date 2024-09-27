7f6fee4643df0e42808c2a40fb8c982b092c2b21
Thibau1t <thibau1t.villepreux@gmail.com>
2024-09-27 22:56:15 +0200

    end of exercice 1 tp1, full test ok, valgrind ok

--- Exercices/tp1/src/nuage.cpp
+++ Exercices/tp1/src/nuage.cpp
@@ -1,5 +1,40 @@
 #include "nuage.hpp"
 
+Cartesien Nuage::barycentre(const Nuage & n) {
+    size_t size = n.size();
+
+    double sum_x = 0.0;
+    double sum_y = 0.0;
+
+    for(size_t i=0; i<size; ++i) {
+
+        Point * point = n.getPoint(i);
+
+        if (dynamic_cast<Cartesien*>(point)) { // if point is Cartesien
+            Cartesien* c = dynamic_cast<Cartesien*>(point);
+            sum_x += c->getX();
+            sum_y += c->getY();
+        } else if (dynamic_cast<Polaire*>(point)) { // if point is Polaire
+            Polaire* p = dynamic_cast<Polaire*>(point);
+            Cartesien c(*p); // convert Polaire to Cartesien
+            sum_x += c.getX();
+            sum_y += c.getY();
+        }
+    }
+
+
+    return (size == 0) ? Cartesien(0, 0) : Cartesien(sum_x / size, sum_y / size);
+}
+
+Cartesien Nuage::BarycentreCartesien::operator()(const Nuage &n) const {
+    return Nuage::barycentre(n);
+}
+
+Polaire Nuage::BarycentrePolaire::operator()(const Nuage &n) const {
+    return Polaire(Nuage::barycentre(n));
+}
+
+
 Nuage::Nuage() {
 }
 
@@ -7,6 +42,10 @@ Nuage::~Nuage() {
     for(Point * p : _points) {
         delete p;
     }
+}
+
+Point * Nuage::getPoint(size_t i) const {
+    return _points.at(i);
 }
 
 void Nuage::ajouter(const Point&  p) {
