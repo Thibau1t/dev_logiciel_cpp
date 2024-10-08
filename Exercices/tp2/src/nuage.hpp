d1ff56b0f6cf03056348bcb61a32b14c6f04753c
Thibau1t <thibau1t.villepreux@gmail.com>
2024-10-08 08:53:28 +0200

    ajoute barycentrev2 avec container générique ok

--- Exercices/tp2/src/nuage.hpp
+++ Exercices/tp2/src/nuage.hpp
@@ -2,7 +2,7 @@
 #define __ZZ__Nuage__hpp
 
 #include <vector>
-#include <typeinfo>
+#include <iterator> // Pour std::iterator_traits
 
 #include "point.hpp"
 #include "cartesien.hpp"
@@ -12,11 +12,13 @@ template <typename T>
 class Nuage
 {
 public:
-    using container_type = typename std::vector<T>;
-    using iterator = typename container_type::iterator;             // Non-const iterator
-    using const_iterator = typename container_type::const_iterator; // Const iterator
+    using container_type = std::vector<T>;
+    using iterator = typename container_type::iterator;
+    using const_iterator = typename container_type::const_iterator;
+
     Nuage() = default;
     ~Nuage() = default;
+
     size_t size() const;
     void ajouter(const T &p);
     iterator begin();
@@ -25,44 +27,44 @@ public:
     const_iterator end() const;
 
 private:
-    container_type _Ts;
+    container_type _points;
 };
 
 template <typename T>
 size_t Nuage<T>::size() const
 {
-    return _Ts.size();
+    return _points.size();
 }
 
 template <typename T>
 void Nuage<T>::ajouter(const T &p)
 {
-    _Ts.push_back(p);
+    _points.push_back(p);
 }
 
 template <typename T>
 typename Nuage<T>::iterator Nuage<T>::begin()
 {
-    return _Ts.begin();
+    return _points.begin();
 }
 
 template <typename T>
 typename Nuage<T>::iterator Nuage<T>::end()
 {
-    return _Ts.end();
+    return _points.end();
 }
 
 template <typename T>
 typename Nuage<T>::const_iterator Nuage<T>::begin() const
 {
-    return _Ts.begin();
-};
+    return _points.begin();
+}
 
 template <typename T>
 typename Nuage<T>::const_iterator Nuage<T>::end() const
 {
-    return _Ts.end();
-};
+    return _points.end();
+}
 
 // Déclaration du template générique
 template <typename T>
@@ -87,6 +89,7 @@ T barycentre_v1(const Nuage<T> &n)
     return T(Cartesien(sumX / size, sumY / size));
 }
 
+// Déclaration de la template spécialisée pour Polaire
 template <>
 Polaire barycentre_v1(const Nuage<Polaire> &nuage)
 {
@@ -109,6 +112,33 @@ Polaire barycentre_v1(const Nuage<Polaire> &nuage)
     double distanceMoyenne = sommeDistances / count;
 
     return Polaire(angleMoyen, distanceMoyenne);
+}
+
+template <typename Container>
+Cartesien barycentre_v2(const Container &nuage)
+{
+    size_t count = 0;
+    double sumX = 0.0;
+    double sumY = 0.0;
+
+    // Itérer à travers les éléments du conteneur
+    for (const auto &point : nuage)
+    {
+        Cartesien c;
+        point.convertir(c); // Convertir chaque point en Cartesien
+        sumX += c.getX();
+        sumY += c.getY();
+        count++;
+    }
+
+    // Si aucun point n'a été ajouté, retourner (0, 0)
+    if (count == 0)
+    {
+        return Cartesien(0.0, 0.0);
+    }
+
+    // Calculer les coordonnées moyennes
+    return Cartesien(sumX / count, sumY / count);
 }
 
 #endif
